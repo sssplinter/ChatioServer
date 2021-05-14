3e0f3c1fa8f17cc4ec4cb2015387320809fb0a3b
Kristina <kristinasementsova21@gmaol.com>
2021-05-14 21:57:47 +0300

    added check unick nikename for new client

--- src/main/kotlin/com/chatio/ClientHandler.java
+++ src/main/kotlin/com/chatio/ClientHandler.java
@@ -5,18 +5,22 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClientHandler implements Runnable {
+public class ClientHandler {
 
     private Server server;
+    private Socket clientSocket;
 
     private PrintWriter outMessage;
-
     private Scanner inMessage;
+
+    private String nickname;
+
+    public String getNickname() {
+        return nickname;
+    }
 
     private static final String HOST = "localhost";
     private static final int PORT = 3443;
-
-    private Socket clientSocket = null;
 
     private static int clientsCount = 0;
 
@@ -25,49 +29,119 @@ public class ClientHandler implements Runnable {
             clientsCount++;
             this.server = server;
             this.clientSocket = socket;
+            System.out.println(socket.getInetAddress().toString());
+
             this.outMessage = new PrintWriter(socket.getOutputStream());
             this.inMessage = new Scanner(socket.getInputStream());
+
+
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = inMessage.nextLine();
+//                        System.out.println(str);
+                        if (str.startsWith("/auth")) {
+                            String[] tokens = str.split("\\s");
+                            //todo если нужно сто-то  для подписти
+                            if (tokens.length == 2) {
+                                System.out.println("Check nickname for uniq.");
+                                if (server.isUniqNick(tokens[1])) {
+                                    nickname = tokens[1];
+                                    System.out.println("[INFO] Успешная авторизация: " + nickname + "; " + socket);
+                                    sendMsg("ok");
+                                    break;
+                                } else {
+                                    System.out.println("пользователь с таким именем уже есть");
+                                    sendMsg("пользователь с таким именем уже есть");
+                                }
+                            } else {
+                                System.out.println("неверный набор токенов");
+                                sendMsg("неверный набор токенов");
+                                //todo
+                            }
+
+                        }
+                    }
+                    while (true) {
+                        String str = inMessage.nextLine();
+                        if (str.startsWith("/authok")) {
+                            server.subscribe(this);
+                            sendMsg("вы вошли в чат");
+                            System.out.println("[INFO] new client connection: ");
+                        }
+                        //todo проверка валидна везде
+                        if (!str.startsWith("/")) {
+                            System.out.println("не правильный индекс");
+                            sendMsg("не правильный индекс");
+                        } else {
+                            if(str.startsWith("/msg")){
+                                String[] tokens = str.split("\\s");
+                                server.sendBroadcastMsg(nickname + ": " + tokens[1]);
+                                System.out.println(nickname + "отправил всем сообщение: " + tokens[1]);
+                            }
+                        }
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                } finally {
+                    try {
+                        inMessage.close();
+                        outMessage.close();
+                        socket.close();
+                    } catch (Exception e) {
+                        //todo errors
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                }
+            }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void run() {
-        try {
-            while (true) {
-                server.sendMessageToAllClients("New member entered the chat.");
-                server.sendMessageToAllClients("Members in chat: " + clientsCount);
-                break;
-            }
-            while (true){
-                if(inMessage.hasNext()){
-                    String clientMessage = inMessage.nextLine();
-                    if(clientMessage.equalsIgnoreCase("##session##end##")){
-                        break;
-                    }
-                    System.out.println(clientMessage);
-                    server.sendMessageToAllClients(clientMessage);
-                }
-            }
-            Thread.sleep(100);
-        } catch (InterruptedException ex){
-            ex.printStackTrace();
-        } finally {
-            this.close();
-        }
-    }
 
-    private void close() {
-        server.removeClient(this);
-        clientsCount--;
-        server.sendMessageToAllClients("Members in the chat: " + clientsCount);
-    }
+//    public void run() {
+//        try {
+//            while (true) {
+//                server.sendBroadcastMsg("New member entered the chat.");
+//                server.sendBroadcastMsg("Members in chat: " + clientsCount);
+//                break;
+//            }
+//            while (true){
+//                if(inMessage.hasNext()){
+//                    String clientMessage = inMessage.nextLine();
+//                    if(clientMessage.startsWith("/private")){
+//                        String[] tokens = clientMessage.split("\\s");
+//                    }
+//                    if(clientMessage.equalsIgnoreCase("##session##end##")){
+//                        break;
+//                    }
+//                    System.out.println(clientMessage);
+//                    server.sendBroadcastMsg(clientMessage);
+//                }
+//            }
+//            Thread.sleep(100);
+//        } catch (InterruptedException ex){
+//            ex.printStackTrace();
+//        } finally {
+//            this.close();
+//        }
+//    }
 
-    public void sendMsg(String msg){
+//    private void close() {
+//        server.removeClient(this);
+//        clientsCount--;
+//        server.sendBroadcastMsg("Members in the chat: " + clientsCount);
+//    }
+
+    public void sendMsg(String msg) {
         try {
-            outMessage.println(msg);
+            outMessage.println(clientSocket + "\n" + msg);
             outMessage.flush();
-        } catch (Exception ex){
+        } catch (Exception ex) {
+            System.out.println("Problem with send messages " + nickname);
             ex.printStackTrace();
         }
     }
--- src/main/kotlin/com/chatio/Server.java
+++ src/main/kotlin/com/chatio/Server.java
@@ -4,27 +4,38 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
     //todo make constant fo r all project
     static final int PORT = 3443;
 
-    private ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
+    private final ArrayList<ClientHandler> clients;
+    private final HashMap<String, ClientHandler> clientsMap;
 
     public Server() {
+        clients = new ArrayList<ClientHandler>();
+        clientsMap = new HashMap<String, ClientHandler>();
+        clientsMap.put("kr", null);
+
         Socket clientSocket = null;
         ServerSocket serverSocket = null;
+        //todo try with arguments
         try {
             serverSocket = new ServerSocket(PORT);
             System.out.println("Server is running.");
 
             while (true) {
+                System.out.println("Wait for clients connection.");
                 clientSocket = serverSocket.accept();
 
                 ClientHandler client = new ClientHandler(clientSocket, this);
-                clients.add(client);
+                System.out.println("clientHandler");
+                //clients.add(client);
+//                clientsMap.put("test", client);
                 // any new client connection processed in a new thread
-                new Thread(client).start();
+//                new Thread(client).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,13 +50,64 @@ public class Server {
         }
     }
 
-    public void sendMessageToAllClients(String msg) {
-        for (ClientHandler client : clients) {
-            client.sendMsg(msg);
+    public void  subscribe(ClientHandler client){
+        if(!(client == null)){
+            sendBroadcastMsg("/log " + client.getNickname() + " connected to the chat.");
+            clientsMap.put(client.getNickname(), client);
+            clients.add(client);
+            client.sendMsg("/log " + client.getNickname() + ", welcome!");
         }
+        //todo refresh people online list
+    }
+
+    public void unsubscribe(ClientHandler client){
+        if(!(client == null)){
+            sendBroadcastMsg("/log" + client.getNickname() + " left the chat.");
+            clients.remove(client);
+            clientsMap.remove(client.getNickname());
+        }
+        //todo refresh people online list
+    }
+
+    public void sendBroadcastMsg(String msg) {
+        for(Map.Entry<String, ClientHandler> client: clientsMap.entrySet()){
+            client.getValue().sendMsg(msg);
+        }
+    }
+
+    //todo trim nick to avoid same nicknames
+    public boolean isUniqNick(String nick){
+        for(Map.Entry<String, ClientHandler> client: clientsMap.entrySet()){
+            if(client.getKey().equals(nick)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void sentPrivateMsg(ClientHandler sender, String recipientName, String msg){
+        ClientHandler recipient = findClient(recipientName);
+        if(recipient != null){
+            recipient.sendMsg("from " + sender.getNickname() + ": " + msg);
+            sender.sendMsg("to " + sender.getNickname() + ": " + msg);
+        } else{
+            //todo /warning?
+            //todo отправление будет по щелчку по клиенту или по вводу никнейма
+            sender.sendMsg("/log " + "There is no any client with name " + recipientName + " int this chat.");
+        }
+    }
+
+    public ClientHandler findClient(String clientName){
+        for(Map.Entry<String, ClientHandler> client: clientsMap.entrySet()){
+            if(client.getKey().equals(clientName)){
+                return client.getValue();
+            }
+        }
+        return null;
     }
 
     public void removeClient(ClientHandler client) {
         clients.remove(client);
+        clientsMap.remove(client);
     }
 }
